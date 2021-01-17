32055a2afd882746abe7799a6e3234602d5389b3
ghandeland <gabriel.handeland@gmail.com>
2021-01-17 02:39:40 +0100

    Added length unit, refactor

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
-
+// Length unit
 const lu = 12;
+
 // Equivalent: 4, 5, 6, 7, 8, 1, 2, 3
 // Directions: E, SE, S, SW, W, NW, N, NE
 let currentDirection = 'E';
@@ -8,15 +9,95 @@ let currentPos = {
   y: 0
 };
 
+let snake = new Snake(240, 240);
+
+function Snake(x, y) {
+  this.x = x;
+  this.y = y;
+  this.dir = 'E';
+  
+  this.coords = [
+    {
+      x: 240,
+      y: 240,
+    },
+    {
+      x: 240,
+      y: 228,
+    }
+  ];
+  
+}
+
+function setup() {
+  createCanvas(50 * lu, 50 * lu);
+  currentPos.x = 25 * lu;
+  currentPos.y = 25 * lu;
+  
+  background(220);
+  frameRate(3);
+  //noLoop();
+  
+  // Draw grid
+  stroke(51);
+  strokeWeight(0.2);
+  for (let i = 0; i < width; i += lu) {
+    line(width, i, 0, i);
+    line(i, 0, i, height);
+  }
+  
+}
+
+function draw() {
+  
+  // Start circle
+  noStroke();
+  fill("green");
+  ellipse(currentPos.x, currentPos.y, 5, 5);
+
+  let num = "4343434343453434";
+
+  
+  let charArray = num.split('');
+  for(let i = 0; i < charArray.length; i++) {
+    if(charArray[i] === '9' || charArray[i] === '0') break;
+    newLine(parseInt(charArray[i]));
+  }
+  
+  // End circle
+  noStroke();
+  fill("red");
+  ellipse(currentPos.x, currentPos.y, 5, 5);
+
+  console.log("hello");
+}
+
+function newLine(number) {
+  stroke(0);
+  strokeWeight(0.6);
+  
+  previousX = currentPos.x;
+  previousY = currentPos.y;
+
+  const coordinates = dir[dirLookups[currentDirection] + (number - 1)];
+  
+  currentPos.x += coordinates.x;
+  currentPos.y += coordinates.y;
+
+  line(previousX, previousY, currentPos.x, currentPos.y);
+  currentDirection = coordinates.n;
+  
+}
+
 const dirLookups = {
-  "E": 0,
-  "SE": 8,
-  "S": 16,
-  "SW": 24,
-  "W": 32,
-  "NW": 40,
-  "N": 48,
-  "NE": 56
+  E: 0,
+  SE: 8,
+  S: 16,
+  SW: 24,
+  W: 32,
+  NW: 40,
+  N: 48,
+  NE: 56,
 };
 
 let dir = [
@@ -93,62 +174,3 @@ let dir = [
   { x: 0, y: lu, n: "SE" },
   { x: -lu, y: lu, n: "SW" },
 ];
-
-
-function setup() {
-  createCanvas(50 * lu, 50 * lu);
-  currentPos.x = 25 * lu;
-  currentPos.y = 25 * lu;
-  
-  background(220);
-
-  // Draw grid
-  stroke(51);
-  strokeWeight(0.2);
-  for (let i = 0; i < width; i += lu) {
-    line(width, i, 0, i);
-    line(i, 0, i, height);
-  }
-}
-
-function draw() {
-  
-  // Start circle
-  noStroke();
-  fill("green");
-  ellipse(currentPos.x, currentPos.y, 5, 5);
-
-  stroke(0);
-  strokeWeight(0.6);
-
-  previousX = currentPos.x;
-  previousY = currentPos.y;
-
-  let num = "434343434343434";
-  let charArray = num.split('');
-  for(let i = 0; i < charArray.length; i++) {
-    if(charArray[i] === '9' || charArray[i] === '0') break; 
-    newLine(parseInt(charArray[i]));
-  }
-  
-  // End circle
-  noStroke();
-  fill("red");
-  ellipse(currentPos.x, currentPos.y, 5, 5);
-
-  noLoop();
-}
-
-function newLine(number) {
-  previousX = currentPos.x;
-  previousY = currentPos.y;
-
-  const coordinates = dir[dirLookups[currentDirection] + (number - 1)];
-  
-  currentPos.x += coordinates.x;
-  currentPos.y += coordinates.y;
-
-  line(previousX, previousY, currentPos.x, currentPos.y);
-  currentDirection = coordinates.n;
-  
-}
